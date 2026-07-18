108f438b926e961fa52331300bea371ecb914935
vgoveas <vgoveas@yahoo.com>
2026-07-18 11:42:32 +0000

    feat: Add GitHub Actions CI/CD pipeline and training script

--- deployment/start.sh
+++ deployment/start.sh
@@ -2,4 +2,4 @@
 #!/bin/bash
 set -x # Print commands and their arguments as they are executed.
 
-python -m streamlit run app.py --server.port 7860 --server.address 0.0.0.0 --logger.level debug
+streamlit run app.py --server.port 7860 --server.address 0.0.0.0 --logger.level debug 1>&2
